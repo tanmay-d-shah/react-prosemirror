4d3a14631e05693338d1d71d90cfe6f51edeb8f3
Tanmay Shah <tanmay.shah@oslash.com>
2023-10-18 22:28:12 +0530

    fix: add curly braces example

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
         <br></br>
         <p className='text-left'>
           <p>In this example, you can insert a Pill (which is a React component) inside a pure javascript based rich text editor library</p>
-          <p>A text can be made into a pill by : </p>
+          <p>A text can be made into a pill by either of the following ways : </p>
           <p>1. Selecting some text inside editor and clicking on curly braces icon from menu of text editor</p>
-          <p>2. Enclosing the text within curly braces</p>
+          <p>2. Enclosing the text within curly braces. Eg: &#123;example-text&#125; </p>
 
           <br></br>
           <p>The pill is for demo purpose. Ideally, we can add any React component in place of pill</p>
